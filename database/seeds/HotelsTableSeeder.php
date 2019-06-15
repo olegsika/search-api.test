640f86440f6ff697a63f15f487c4a65af2e779a9
olech3000@gmail.com <301276o>
2019-06-15 22:59:13 +0300

    Create csv parser for seeder

--- database/seeds/HotelsTableSeeder.php
+++ database/seeds/HotelsTableSeeder.php
@@ -4,6 +4,10 @@ use Illuminate\Database\Seeder;
 
 class HotelsTableSeeder extends Seeder
 {
+    private $fileUri = "database/csv/property-data.csv";
+
+    private $keys = ['name', 'price', 'bedrooms', 'bathrooms', 'storeys', 'garages'];
+
     /**
      * Run the database seeds.
      *
@@ -11,6 +15,19 @@ class HotelsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        if (!file_exists(base_path($this->fileUri))) return;
+
+        $this->import($this->fileUri);
+    }
+
+    private function import($file)
+    {
+        $allData = array_map('str_getcsv', file(base_path($file)));
+        array_shift($allData);
+        foreach ($allData as $row) {
+            $data = array_combine($this->keys, $row);
+
+            DB::table('hotels')->insert($data);
+        }
     }
 }
