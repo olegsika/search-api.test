f082de8a881dda71a0bc4942055dbb2beb86dd56
olech3000@gmail.com <301276o>
2019-06-15 21:41:44 +0300

    Connect vue.js create migration, seed, controller, model, route

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/get', 'HotelController@getHotels');
+Route::put('/search', 'HotelController@search');
